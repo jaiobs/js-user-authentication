50b6f939902369a36f697723f47863331b3b1b7b
rafee11 <mohamedrafiudeen@optisolbusiness.com>
2020-10-14 14:34:14 +0530

    User authentication added

---
+++ src/environments/environment.mock.ts
@@ -0,0 +1,14 @@
+export const environment = {
+    production: false,
+    apiBaseUrl: 'http://127.0.0.1:3000/api/',
+    firebase: {
+        apiKey: '',
+        authDomain: '',
+        databaseURL: '',
+        projectId: '',
+        storageBucket: '',
+        messagingSenderId: '',
+        appId: '',
+        measurementId: '',
+    },
+};
